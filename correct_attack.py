5c5592af68c9c6e72dac970b780ae5bc29f9ce17
XuanZhao777 <149707203+XuanZhao777@users.noreply.github.com>
2023-12-17 20:08:28 +0800

    Update correct_attack.py

--- correct_attack.py
+++ correct_attack.py
@@ -1,11 +1,32 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+
+def correct_attack(model, original_image, perturbed_image, max_iterations=100, lr=0.01):
+    # Ensure the model is in evaluation mode
+    model.eval()
+
+    # Clone the perturbed image and enable gradient computation
+    perturbed_image = perturbed_image.clone().detach()
+    perturbed_image.requires_grad = True
+
+    # Choose an optimizer
+    optimizer = optim.Adam([perturbed_image], lr=lr)
+
+    for iteration in range(max_iterations):
+        # Reset gradients
+        optimizer.zero_grad()
+
+        # Get the model outputs
+        perturbed_output = model(perturbed_image)
+        original_output = model(original_image)
+
+        # Define the loss function
+        loss = nn.MSELoss()(perturbed_output, original_output)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
 
 
-def correct_attack(original_image, attacked_part, correction_factor=0.5):
-    # 复制原始图像
-    corrected_image = original_image.clone()
-
-    # 对受到攻击的部分进行校正恢复
-    corrected_image[attacked_part == 1] = original_image[attacked_part == 1] * correction_factor
-
-    return corrected_image
+    return perturbed_image
