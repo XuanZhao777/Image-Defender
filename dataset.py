7811935150063b76f465f08558d0c8d89b1da612
XuanZhao777 <149707203+XuanZhao777@users.noreply.github.com>
2023-12-17 20:03:00 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -4,17 +4,18 @@ from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
 from PIL import Image
 
-# 定义数据集的转换
+# Define transformations for the dataset
 transform = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-# 下载CIFAR-10数据集
+# Download the CIFAR-10 dataset
 cifar10_dataset = CIFAR10(root='./data', train=True, download=True, transform=transform)
 
-# 使用数据加载器加载数据
+# Use a data loader to load the data
 data_loader = DataLoader(cifar10_dataset, batch_size=1, shuffle=True)
 
-# 从数据加载器中获取一张图像
+# Get one image from the data loader
 original_image, original_label = next(iter(data_loader))
+
 
