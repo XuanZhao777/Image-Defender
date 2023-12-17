de2b1846653564899ad451d0117cb2d51a81a52e
XuanZhao777 <149707203+XuanZhao777@users.noreply.github.com>
2023-12-17 20:01:00 +0800

    Update and rename pack.py to App.py

--- App.py
+++ App.py
@@ -13,12 +13,14 @@ from combine_parts import combine_parts
 from test import test
 
 class ImageDisplayApp:
-    def __init__(self, root, model, epsilon, threshold, correction_factor):
+    def __init__(self, root, model, epsilon, threshold, correction_factor, alpha, iterations):
         self.root = root
         self.model = model
         self.epsilon = epsilon
         self.threshold = threshold
         self.correction_factor = correction_factor
+        self.alpha = alpha
+        self. iterations = iterations
 
         self.transform = transforms.Compose([transforms.ToTensor()])
 
@@ -30,7 +32,7 @@ class ImageDisplayApp:
         self.setup_gui()
 
     def setup_gui(self):
-        self.root.title("Adversarial Attack Demo")
+        self.root.title("Image Defender")
 
         self.show_original_image()
         self.show_adversarial_image()
@@ -42,29 +44,45 @@ class ImageDisplayApp:
         self.stop_button = tk.Button(self.root, text="Stop", command=self.root.destroy)
         self.stop_button.grid(row=1, column=2, padx=10, pady=10)
 
-    def show_image(self, image, title, size=(300, 300), row=None, col=None):
+    def setup_gui(self):
+        self.root.title("Image Defender")
+
+        self.show_original_image()
+        self.show_adversarial_image()
+        self.show_combined_image()
+
+        self.next_button = tk.Button(self.root, text="Next", command=self.load_next_image)
+        self.next_button.grid(row=2, column=1, padx=10, pady=10)  # Adjusted row to avoid overlapping with the label
+
+        self.stop_button = tk.Button(self.root, text="Stop", command=self.root.destroy)
+        self.stop_button.grid(row=2, column=2, padx=10, pady=10)  # Adjusted row to avoid overlapping with the label
+
+    def show_image(self, image, title, size=(200, 200), row=None, col=None):
         img = Image.fromarray((image.squeeze().permute(1, 2, 0).detach().numpy() * 255).astype('uint8'))
         img = ImageTk.PhotoImage(img.resize(size))
         panel = tk.Label(self.root, image=img)
         panel.image = img
         panel.grid(row=row, column=col, padx=10, pady=10)
-        plt.title(title)
+
+        title_label = tk.Label(self.root, text=title)
+        title_label.grid(row=row + 1, column=col, padx=10, pady=5)
 
     def show_original_image(self):
         self.show_image(self.original_image, "Original Image", row=0, col=0)
 
     def show_adversarial_image(self):
         ground_truth_labels = torch.tensor([2])
-        perturbed_image = adversarial_attack(self.model, self.original_image, label=ground_truth_labels, epsilon=self.epsilon)
+        perturbed_image = adversarial_attack(self.model, self.original_image, label=ground_truth_labels,
+                                             epsilon=self.epsilon, alpha=self.alpha, iterations=self.iterations)
         self.show_image(perturbed_image, "Adversarial Image", row=0, col=1)
 
     def show_combined_image(self):
         ground_truth_labels = torch.tensor([2])
-        perturbed_image = adversarial_attack(self.model, self.original_image, label=ground_truth_labels, epsilon=self.epsilon)
-        attacked_part, unattacked_part = separate_attack(self.model, self.original_image, perturbed_image, threshold=self.threshold)
-        corrected_part = correct_attack(self.original_image, attacked_part)
-        not_attacked_part = 1 - attacked_part
-        combined_image = combine_parts(self.original_image, not_attacked_part, corrected_part)
+        perturbed_image = adversarial_attack(self.model, self.original_image, label=ground_truth_labels,
+                                             epsilon=self.epsilon, alpha=self.alpha, iterations=self.iterations)
+        attacked_part, unattacked_part = separate_attack(self.model, self.original_image, perturbed_image)
+        corrected_part = correct_attack(self.model, self.original_image, attacked_part, max_iterations=100, lr=0.01)
+        combined_image = combine_parts(self.original_image, unattacked_part, corrected_part)
         test_result = test(combined_image, self.original_image)
         self.show_image(combined_image, f"Combined Image\nTest Result: {test_result}", row=0, col=2)
 
@@ -77,14 +95,17 @@ class ImageDisplayApp:
         except StopIteration:
             print("No more images in the dataset.")
 
+
 if __name__ == "__main__":
     model = resnet18(weights='IMAGENET1K_V1')
     model.eval()
 
-    epsilon = 0.1
+    epsilon = 0.03
     threshold = 0.1
     correction_factor = 0.5
+    alpha = 0.0015
+    iterations = 80
 
     root = tk.Tk()
-    app = ImageDisplayApp(root, model, epsilon, threshold, correction_factor)
+    app = ImageDisplayApp(root, model, epsilon, threshold, correction_factor, alpha, iterations)
     root.mainloop()
