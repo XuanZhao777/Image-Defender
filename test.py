8e7542dc1a04a05fcc193afa388231be07d5a252
XuanZhao777 <149707203+XuanZhao777@users.noreply.github.com>
2023-12-17 20:05:10 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,20 +2,20 @@ import numpy as np
 import cv2
 
 def test(combined_image, original_image):
-    # 将PyTorch Tensor转换为NumPy数组
+    # Convert PyTorch Tensor to NumPy arrays
     combined_array = combined_image.detach().cpu().numpy()
     original_array = original_image.detach().cpu().numpy()
 
-    # 计算均方误差
+    # Calculate Mean Squared Error
     mse = np.mean((original_array - combined_array) ** 2)
 
-    # 计算PSNR
+    # Calculate PSNR
     psnr = 20 * np.log10(255 / np.sqrt(mse))
 
-    # 打印评估结果
+    # Print evaluation results
     print("<70dB: Poor quality,\n"
           ">70dB: Good quality")
     print(f'Our PSNR: {psnr:.2f} dB')
 
-    # 返回PSNR值
+    # Return the PSNR value
     return psnr
