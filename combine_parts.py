b52a8f3a42a0d4b340ed64141ac92d13d6f1d368
XuanZhao777 <149707203+XuanZhao777@users.noreply.github.com>
2023-12-17 20:01:47 +0800

    Update combine_parts.py

--- combine_parts.py
+++ combine_parts.py
@@ -2,20 +2,37 @@ import torch
 import numpy as np
 
 def combine_parts(original_image, not_attacked_part, corrected_part):
-    # 将 PyTorch 张量转换为 NumPy 数组以便更容易进行操作
+    # Convert PyTorch tensors to NumPy arrays
     original_array = original_image.detach().cpu().numpy()
     not_attacked_array = not_attacked_part.detach().cpu().numpy()
     corrected_array = corrected_part.detach().cpu().numpy()
 
-    # 找到校正部分和原始图像之间的差异
-    differences = np.where(not_attacked_array  != original_array)
+    # Ensure all arrays are 3-dimensional (C, H, W)
+    if original_array.ndim == 4:
+        original_array = original_array.squeeze(0)
+    if not_attacked_array.ndim == 4:
+        not_attacked_array = not_attacked_array.squeeze(0)
+    if corrected_array.ndim == 4:
+        corrected_array = corrected_array.squeeze(0)
 
-    # 用 not_attacked_array 中的相应部分替换 corrected_array 中的不同部分
-    combined_array = np.copy(not_attacked_array)
-    combined_array[differences] = corrected_array[differences]
+    # Print shapes for debugging purposes
+    print("Original array shape:", original_array.shape)
+    print("Not attacked array shape:", not_attacked_array.shape)
+    print("Corrected array shape:", corrected_array.shape)
 
-    # 将合并后的数组转换回 PyTorch 张量
+    # Ensure all arrays have the same shape
+    if original_array.shape != not_attacked_array.shape or original_array.shape != corrected_array.shape:
+        raise ValueError("All input arrays must have the same shape.")
+
+    # Directly add the not attacked part and corrected part, subtract the overlapping part
+    combined_array = not_attacked_array + corrected_array - original_array
+
+    # Clip the result to ensure values are in a valid range
+    combined_array = np.clip(combined_array, 0, 1)
+
+    # Convert the combined array back to a PyTorch tensor
     combined_tensor = torch.from_numpy(combined_array)
 
     return combined_tensor
+
 
